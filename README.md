295a6b5f6ae1b0c42685931ca8349bebf79ebed8
farhad_de <farhad_deh@yahoo.com>
2024-10-28 16:26:20 +0330

    fix: readme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # MIMS - Material Inventory Management System
 
-**MIMS** is a Node.js-powered web application designed to help manage material inventories efficiently. It provides users with tools to track materials, view inventory levels, and leverage APIs for extended functionality.
+**MIMS** is a Node.js powered web application designed to help manage material inventories efficiently. It provides users with tools to track materials, view inventory levels, and leverage APIs for extended functionality.
+
+![Node.js](https://img.shields.io/badge/Node.js-339933?logo=Node.js&logoColor=white) ![Express.js](https://img.shields.io/badge/Express.js-000000?logo=express&logoColor=fff&style=flat) ![Swagger](https://img.shields.io/badge/-Swagger-%23Clojure?logo=Swagger&logoColor=white) 
 
 ## Features
 
@@ -8,3 +10,47 @@
 - **API Documentation**: Access API documentation via Swagger for easy integration.
 - **Bilingual Interface**: Supports English and Persian languages.
 - **Built with Express and Node.js**: A reliable backend framework for robust performance.
+
+## Installation
+
+1. **Clone the repository:**
+
+   ```bash
+   git clone https://github.com/your-username/MIMS.git
+
+   ```
+
+2. **Navigate to the project directory:**
+
+   ```bash
+   cd MIMS
+
+   ```
+
+3. **Install dependencies:**
+
+   ```bash
+   npm install
+
+   ```
+
+4. **Configure Environment Variables:**
+   Create a .env file in the root directory and add the following:
+   ```bash
+   PORT=3000
+   URL=http://localhost:3000
+   ```
+
+5. **Install dependencies:**
+
+   ```bash
+   npm start
+
+   ```
+
+6. **Access the application:**
+
+   ```bash
+     Visit http://localhost:3000 in your browser.
+
+   ```
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const swaggerUI = require("swagger-ui-express");
 const swaggerSpec = require("./swagger");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const Url = process.env.URL || `http://localhost:${PORT}`;
@@ -25,17 +25,33 @@ app.get("/", (req, res) => {
           body { font-family: Arial, sans-serif; display: flex; align-items: center; justify-content: center; height: 100vh; margin: 0; background-color: #f4f4f9; }
           .container { text-align: center; padding: 20px; border-radius: 8px; background-color: #fff; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); }
           h1 { color: #333; }
-          p { color: #666; font-size: 1.1em; }
-          a { color: #007bff; text-decoration: none; }
+          p { color: #666; font-size: 1.1em;    margin-bottom: 25px; }
+          .url { color: #007bff; text-decoration: none; }
           span { color: #78909C; text-decoration: none; }
+          .styled-link {
+            background-color: #66BB6A; 
+            color: white;
+            padding: 10px 16px;  
+            margin: 30px;  
+            text-decoration: none;
+            border-radius: 5px;
+            font-size: 18px;
+            transition: background-color 0.3s ease, transform 0.3s ease; 
+            box-shadow: 0 3px 7px rgba(0, 0, 0, 0.2); 
+        }
+
+        .styled-link:hover {
+            background-color: #7BBF3A; 
+            transform: translateY(-2px); 
+        }
         </style>
       </head>
       <body>
         <div class="container">
           <h1>Material Inventory Management System</h1>
           <h1>به سیستم مدیریت موجودی مواد خوش آمدید</h1>
-          <p>برای بررسی API ها از ابزار Swagger در   <a href="${Url}/docs" >این صفحه</a> استفاده کنید.</p>
-          <span>قدرت گرفته از Express + Node Js</span>
+          <p>برای بررسی API ها از ابزار Swagger در   <a class="url" href="${Url}/docs" >این صفحه</a> استفاده کنید.</p>
+           <a href="https://github.com/farhad-deh/MIMS" class="styled-link" >صفحه ی گیت هاب پروژه </a>
         </div>
       </body>
       </html>
